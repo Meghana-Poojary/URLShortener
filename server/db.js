91da81fcd82796907f4039872dc0838db9f3604e
Meghana Poojary <meghanapoojary121@gmail.com>
2026-02-21 21:02:54 +0530

    Redirection error

--- server/db.js
+++ server/db.js
@@ -4,13 +4,18 @@ import dotenv from "dotenv";
 const { Pool } = pkg;
 dotenv.config();
 
+// export const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: process.env.NODE_ENV === "production"
+//         ? { rejectUnauthorized: false }
+//         : false
+// });
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 // export const pool = new Pool({
 //     user: process.env.DB_USER,
 //     host: process.env.DB_HOST,
